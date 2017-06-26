2dda6b7e9128705882708fce781d66dc0a43d0a0
TANYA WISE <tanya.wise@gvs.ga.gov>
2017-06-26 19:59:45 -0400

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'welcome/about'
+
+  get 'contact' => 'welcome#contact'
+
+  get 'home' => 'welcome#index'
+
+  get 'about' => 'welcome#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
